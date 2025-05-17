54c3e13411cd70319cb29dd49f4aa689e09db499
ArielMn22 <arielpaixao10@gmail.com>
2025-05-17 10:08:50 -0300

    feat: implementar carregamento dinâmico de tags no painel esquerdo

--- src/components/left-panel/index.jsx
+++ src/components/left-panel/index.jsx
@@ -1,8 +1,26 @@
 import { faBoxArchive, faHouse, faTag } from "@fortawesome/free-solid-svg-icons";
 import HomeIcon from "../../assets/imgs/Home.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
 function LeftPanel({ enviarTag }) {
+
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+
+    getTags();
+
+  }, []);
+
+  const getTags = async () => {
+
+    const response = await fetch('http://localhost:3000/tags');
+    const data = await response.json();
+
+    setTags(data);
+
+  }
 
   return (
     <nav className="left-panel">
@@ -27,30 +45,12 @@ function LeftPanel({ enviarTag }) {
 
         <span className="tag">Tags</span>
 
-        <button onClick={() => enviarTag('Cooking')}>
-          <FontAwesomeIcon icon={faTag} className="icon" />
-          Cooking
-        </button>
-
-        <button>
-          <FontAwesomeIcon icon={faTag} className="icon" />
-          Cooking
-        </button>
-
-        <button>
-          <FontAwesomeIcon icon={faTag} className="icon" />
-          Cooking
-        </button>
-
-        <button>
-          <FontAwesomeIcon icon={faTag} className="icon" />
-          Cooking
-        </button>
-
-        <button>
-          <FontAwesomeIcon icon={faTag} className="icon" />
-          Cooking
-        </button>
+        {tags.map((tag) => (
+          <button key={tag.id} onClick={() => enviarTag(tag.name)}>
+            <FontAwesomeIcon icon={faTag} className="icon" />
+            {tag.name}
+          </button>
+        ))}
 
       </div>
 
